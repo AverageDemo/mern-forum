18e9c3460a0be71b6bc1b03c69c2bdfc4372ebec
AverageDemo <average.demo@gmail.com>
2018-12-31 23:47:03 -0500

    started redux implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -13,17 +15,19 @@ import "./App.css";
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div className="App">
-                    <div className="d-flex flex-column content-box">
-                        <Navbar />
-                        <Route exact path="/" component={Landing} />
-                        <Route exact path="/login" component={Login} />
-                        <Route exact path="/register" component={Register} />
-                        <Footer />
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <div className="d-flex flex-column content-box">
+                            <Navbar />
+                            <Route exact path="/" component={Landing} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/register" component={Register} />
+                            <Footer />
+                        </div>
                     </div>
-                </div>
-            </Router>
+                </Router>
+            </Provider>
         );
     }
 }
